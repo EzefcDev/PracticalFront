02c3cfb36535690955e4edb74c8d624e5b8ed322
WORK987\ezefe <ezeferrauti@gmail.com>
2023-05-12 23:46:20 -0300

    new project todo

---
+++ ToDo/index.js
@@ -0,0 +1,36 @@
+const list = document.getElementById('list')
+const buttonAdd = document.getElementById('add')
+const inputText = document.getElementById('input-text')
+const trList = document.getElementsByTagName('tr')
+const buttonClear = document.getElementsByTagName('button')
+let id = 0
+let jobs = []
+
+buttonAdd.addEventListener('click',()=>{
+    let job = inputText.value
+    if (job !== '') {
+        id++
+        jobs.push({
+            id,
+            job,
+            status:false
+        })
+        let tr = document.createElement('tr')
+        tr.innerHTML = `<th>${id}</th><th>${job}</th><th><input onclick="check(${id})" type="checkbox"></th><th><button onclick="clear(${id})">borrar</button></th>`
+        list.appendChild(tr)
+    }
+})
+
+
+const clear = (id) => {
+    trList[id].classList.add("invisible")
+}
+
+const check = (id) => {
+    const newJobs = jobs.filter(item => {
+        if(item.id === id ){
+            item.status = !item.status
+        }
+        return item
+    })
+}
